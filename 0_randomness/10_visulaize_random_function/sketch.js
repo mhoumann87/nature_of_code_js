5e43e607722d148c67393292d40ac1f47aacd901
Michael Houmann <mhoumann87@gmail.com>
2025-03-18 13:38:03 +0100

    Add sketch to vizualize random function

---
+++ 0_randomness/10_visulaize_random_function/sketch.js
@@ -0,0 +1,20 @@
+/* 
+  Author Michael Houmann
+  Created 18. March 2025
+  
+  Visualization of the random() function
+*/
+
+function setup() {
+  createCanvas(640, 400);
+}
+
+function draw() {
+  background(0);
+  stroke(220);
+  fill(220);
+  // Get the random number
+  const x = random(0, width);
+  // Draw a circle for the value of tha random number
+  circle(x, height / 2, 20);
+}

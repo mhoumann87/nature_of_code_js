8c6425e6b002d95c071a2d5ab63b9c43e41134ed
Michael Houmann <mhoumann87@gmail.com>
2025-04-06 10:44:23 +0200

    Add project and update README

---
+++ 0_randomness/15-noise-graph/sketch.js
@@ -0,0 +1,15 @@
+/* 
+  Author Michael Houmann
+  Created April 5 2025
+
+  A graph that shows how the random function "works"
+*/
+
+function setup() {
+  createCanvas(640, 400);
+}
+
+function draw() {
+  background(51);
+  
+}
---
+++ 0_randomness/16-2d-perlin-noise-field/sketch.js
@@ -0,0 +1,18 @@
+/* 
+  Author Michael Houmann
+  Created April 6 2025
+
+  Find the video tutorials on Perlin Noise by Coding Train
+    https://youtu.be/Qf4dIN99e2w?feature=shared
+    https://youtu.be/YcdldZ1E9gU?feature=shared
+    https://youtu.be/y7sgcFhk6ZM?feature=shared
+
+*/
+
+function setup() {
+  createCanvas(640, 400);
+}
+
+function draw() {
+  background(0);
+}

1a155433df814e58d93e3e380bda479b7d741155
Michael Houmann <mhoumann87@gmail.com>
2025-04-14 11:15:03 +0200

    Add exercise 0-10

---
+++ 0_randomness/19-exercise-0-10-landscape/sketch.js
@@ -0,0 +1,31 @@
+/* 
+  Author Michael Houmann
+  Created April 14 2025
+
+  Exercise 0.10
+  Use the noise values as the elevations of a landscape.
+*/
+
+// Global variables for our elements
+let land;
+let theta = 0.0; // angle
+
+function setup() {
+  createCanvas(640, 240, WEBGL);
+
+  // Initiate a new landscape object
+  land = new Terrain(20, 800, 400);
+}
+
+function draw() {
+  land.calculate();
+  background(255);
+  push();
+  translate(0, 20, -200);
+  rotateX(PI / 3);
+  rotateZ(theta);
+  land.render();
+  pop();
+
+  theta += 0.0025;
+}

dac21e33be433c86fbfa1ddff9ff85fca686e75f
Michael Houmann <mhoumann87@gmail.com>
2025-04-30 13:27:36 +0200

    Worked on the grid

--- 0_randomness/20-extra-coding-challenge-flow-field/sketch.js
+++ 0_randomness/20-extra-coding-challenge-flow-field/sketch.js
@@ -12,14 +12,16 @@
 */
 
 // Global variable for the increment value we use
-const increment = 0.01;
+const increment = 0.1;
 // A global variable that hold the size of each field
-const scale = 20;
+const scale = 10;
 // And we need to have some columns and rows;
 let cols, rows;
+// A variable for framerate
+let fr;
 
 function setup() {
-  createCanvas(640, 400);
+  createCanvas(200, 200);
   // To get it to look alike on all screen resolutions
   // pixelDensity(1);
 
@@ -27,22 +29,29 @@ function setup() {
   // We floor the result to be sure to not have any decimal points
   cols = floor(width / scale);
   rows = floor(height / scale);
+
+  // Just to check the framerate
+  fr = createP('');
 }
 
   function draw() {
     let yOffset = 0;
 
-    for (let y = 0; y < innerHeight; y++) {
+    for (let y = 0; y < rows; y++) {
       let xOffset = 0;
-      for (let x = 0; x < width; x++) {
+      for (let x = 0; x < cols ; x++) {
         // remember 4 pixels for every one in our array
         const index = (x + y * width) * 4;
         // Now we can base our color on the noise function
         let color = noise(xOffset, yOffset) * 255;
-        
+        fill(color);
+        rect(x * scale, y * scale, scale, scale);
+
         xOffset += increment;
       }
       yOffset += increment;
     }
+
+    fr.html(floor(frameRate()));
   }
   
